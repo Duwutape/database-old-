1412b6eb111a32e45c95b15551a67ff1b1f92abe
Duwutape <quinn.langheld@gmx.de>
2022-11-10 18:25:27 +0100

    deleted unused code

--- src/main/java/meth/SwingMeth.java
+++ src/main/java/meth/SwingMeth.java
@@ -14,21 +14,6 @@ public class SwingMeth {
         c.gridwidth = gridwidth;
         panel.add(component, c);
     }
-    public static void addToPanel(JPanel panel, JComponent component, double weigthx, int gridx, int gridy, int gridwidth, boolean isSep) {
-        GridBagConstraints c = new GridBagConstraints();
-
-        c.weightx = weigthx;
-        c.gridx = gridx;
-        c.gridy = gridy;
-        c.gridwidth = gridwidth;
-
-        if (isSep) {
-            c.gridheight = 10;
-        } else {
-            c.gridheight = 1;
-        }
-        panel.add(component, c);
-    }
 
     public static void sepToPanel(JPanel panel, JComponent component, int gridx, int gridy){
         GridBagConstraints c = new GridBagConstraints();
