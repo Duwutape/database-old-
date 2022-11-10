0d964dfa7ec7d462c61ec3078ef4a0829a519197
Duwutape <quinn.langheld@gmx.de>
2022-11-10 13:33:56 +0100

    added editseries

--- src/main/java/gui/Menu.java
+++ src/main/java/gui/Menu.java
@@ -308,7 +308,7 @@ public class Menu extends JFrame {
         editSeries.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //new EditSeries();
+                new EditSeries();
             }
         });
 
